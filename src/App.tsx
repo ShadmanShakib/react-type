14d790f8873786839d8e8d5be76505f8f28bd62b
SaadShakib <imsaadshakib@gmail.com>
2021-01-10 08:17:34 +0600

    graphql removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,12 @@
-import './App.css';
-
-import {ApolloProvider, ApolloClient, InMemoryCache,gql, createHttpLink} from '@apollo/client';
-
-
-
-const client = new ApolloClient({
-  uri: 'http://localhost:1337/graphql',
-  cache: new InMemoryCache()
-});
-
-
-client
-  .query({
-    query: gql`
-      query  {
-     universities{
-       Name
-     }
-        }
-    `
-  })
-  .then(result => console.log(result));
-
 function App() {
 
 return(
-  <ApolloProvider client={client}>
   <div>
  
     HELLO WORLD
    
   </div>
-  </ApolloProvider>
+ 
 )
 }
 
